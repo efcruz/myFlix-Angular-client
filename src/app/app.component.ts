7e530f26749875eadcb95a85a38ba65361b0fd1f
efcruz <eduarda.web.dev@gmail.com>
2022-08-27 14:49:26 +0200

    login form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
 import { MatDialog } from '@angular/material/dialog';
 
 @Component({
@@ -19,4 +20,13 @@ export class AppComponent {
       });
     }
 
+  
+   // This is the function that will open the dialog when login button is clicked
+ 
+  openUserLoginDialog(): void {
+    this.dialog.open(UserLoginFormComponent, {
+      // Assign dialog width
+      width: '280px'
+    });
+  }
 }
